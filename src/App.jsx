a36e94aaeee072d1352aa3762817ad5f064204f4
Roberto <roberto.quadraccia90@gmail.com>
2023-12-05 16:41:51 +0100

    Value update state created

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import Player from "./components/Player";
 
+// Whenever we are using or reusing a component, React will basically create a new isolated instance.
+// So even though both these players here use the same player component, they work totally isolated from each other.
+// If the state in this first player component instance here changes, 
+// the second player component instance does not care about that at all.
+
 const App = () => {
   return (
     <main>
       <div id="game-container">
         <ol id="players">
-          <Player name="Player 1" symbol="X" />
-          <Player name="Player 2" symbol="O" />
+          <Player initialName="Player 1" symbol="X" />
+          <Player initialName="Player 2" symbol="O" />
         </ol>
         GAME BOARD
       </div>
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,43 @@
-const Player = ({ name, symbol }) => {
+import { useState } from "react";
+
+const Player = ({ initialName, symbol }) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [playerName, setPlayerName] = useState(initialName);
+
+  // When updating your state based on the PREVIOUS VALUE of that state,
+  // you should pass a function to that state updating function.
+  // This is a strong recommendation by the React team.
+  // It's a best practice in the React world and it is simply something you should memorise.
+
+  //  setIsEditing(!isEditing); // => schedules a state update to true (not immediately)
+  //  setIsEditing(!isEditing); // => false (since the above hasn't update yet, this will schedule to true too)
+  //  the above way is not correct because React is scheduling the state update, although it's really fast, it may not work.
+
+  const handleEditClick = () => {
+    setIsEditing((editing) => !editing); // right way to do it, you simply have a guarantee by React that you'll always be working
+    // with the latest available state value.
+  };
+
+  const handleChange = (e) => {
+    setPlayerName(e.target.value); // We target the value property in the <input>
+  };
+
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        {!isEditing ? (
+          <span className="player-name">{playerName}</span>
+        ) : (
+          <input
+            type="text"
+            required
+            value={playerName}
+            onChange={handleChange}
+          ></input>
+        )}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={handleEditClick}>{!isEditing ? "Edit" : "Save"}</button>
     </li>
   );
 };
